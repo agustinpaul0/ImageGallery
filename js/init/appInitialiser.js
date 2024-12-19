b63aaeefee6509325b42f8b2bc01f057e0869ec1
Agustin Paul <a.agustinpaul@gmail.com>
2024-12-18 22:51:44 -0300

    Fixing the initial settings

--- js/init/appInitialiser.js
+++ js/init/appInitialiser.js
@@ -1,7 +1,7 @@
 import { setupEventHandlers } from '../ui/eventHandlers.js';
 
 export function initApp() {
-  //localStorage.clear();
-  //console.log("Borrar clear en appInitialiser");
+  localStorage.clear();
+  console.log("Borrar clear en appInitialiser");
   setupEventHandlers();
 }
